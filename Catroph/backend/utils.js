27f56b577ab7bbbbae4638276cc25391e0a96817
chevano <chevanogordon@gmail.com>
2021-04-01 01:13:40 -0400

    Update utils.js

--- Catroph/backend/utils.js
+++ Catroph/backend/utils.js
@@ -16,4 +16,33 @@ export const generateToken = (user) => {
             expiresIn: "30d", // expiration is in 30 days(timestamp which is optional)
         }
     );
-}
+};
+
+// Middleware to authenticate user
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(authorization){
+        // format: Bearer XXXXX, where XXXXX is the token
+        const token = authorization.slice(7, authorization.length);
+        // decrypt the token(decode contains the data inside token)
+        jwt.verify(token, process.env.JWT_SECRET || "mySecret", (err, decode) => {
+            // Checks if token is invalid
+            if(err)
+                res.status(401).send({ message: "Invalid Token" });
+            else {
+                req.user = decode;
+                next(); // Passes user as a property of request to the next middleware
+            }
+        })
+    }
+    else
+        res.status(401).send({ message: "No Token" });    
+};
+
+// Middleware to authenticate Admin
+export const isAdmin = (req, res, next) => {
+    if(req.user && req.user.isAdmin)
+        next();
+    else
+        res.status(401).send({ message: "Invalid Admin Token"});
+};
