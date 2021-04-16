b1fef0fc37db66566fd824e815ea6f451327227f
chevano <chevanogordon@gmail.com>
2021-04-16 01:45:24 -0400

    Update utils.js

--- Catroph/backend/utils.js
+++ Catroph/backend/utils.js
@@ -1,4 +1,9 @@
 import jwt from 'jsonwebtoken';
+import mg from 'mailgun-js';
+import dotenv from 'dotenv';
+
+// Allows us to use the dotenv environment variables file
+dotenv.config();
 
 // Generates a hash string that is used for your next request to authenticate 
 // your current request which provides a way to authenticates the user
@@ -62,4 +67,79 @@ export const isSellerOrAdmin = (req, res, next) => {
         next();
     else
         res.status(401).send({ message: "Invalid Seller/Admin Token"});
+};
+
+export const mailgun = () => mg({
+    apiKey: process.env.MAILGUN_API_KEY,
+	domain: process.env.MAILGUN_DOMAIN,
+});
+
+export const payOrderEmailTemplate = (order) => {
+    return `<h1>Thanks for shopping with us</h1>
+        <p>Hello ${order.user.name},</p>
+        <p>We have finished processing your order</p>
+        <h2>[Order ${order._id}] (${order.createdAt.toString().substring(0,10)})</h2>
+
+        <table>
+            <thead>
+                <tr>
+                    <td> <strong>Product</strong></td>
+                    <td> <strong>Quantity</strong></td>
+                    <td> <strong align="right">Price</strong></td>
+                </tr>
+            </thead>
+
+            <tbody>
+                ${order.orderItems.map((item) => 
+                `
+                <tr>
+                    <td>${item.name}</td>
+                    <td align="center">${item.qty}</td>
+                    <td align="right">$${item.price.toFixed(2)}</td>
+                </tr>
+                `
+                ).join("\n")}
+            </tbody>
+        
+            <tfoot>
+                <tr>
+                    <td colspan="2">Items Price:</td>
+                    <td align="right">$${order.itemsPrice.toFixed(2)}</td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">Tax Price:</td>
+                    <td align="right">$${order.taxPrice.toFixed(2)}</td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">Shipping Price:</td>
+                    <td align="right">$${order.shippingPrice.toFixed(2)}</td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">Total Price:</td>
+                    <td align="right"><strong> $${order.totalPrice.toFixed(2)}</strong></td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">Payment Method:</td>
+                    <td align="right">${order.paymentMethod}</td>
+                </tr>
+            </tfoot>
+        </table>
+        
+        <h2>Shipping Address</h2>
+        <p>
+            ${order.shippingAddress.fullName}, <br/>
+            ${order.shippingAddress.address}, <br/>
+            ${order.shippingAddress.city}, <br/>
+            ${order.shippingAddress.country}, <br/>
+            ${order.shippingAddress.postalCode}, <br/>
+        </p>
+
+        <hr/>
+
+        <p>Thanks for shopping with us.</p>
+        `
 };
